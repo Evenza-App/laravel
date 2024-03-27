92e2b108c155e0fb483b09d31dd187350d48f6a6
zienadev <zeinaabdahmad77@gmail.com>
2024-03-27 20:11:44 +0300

    commit 3

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,7 +28,6 @@ class User extends Authenticatable implements FilamentUser
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
     ];
